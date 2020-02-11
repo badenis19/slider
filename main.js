abeac64f8362d645e5e7e96872f43ec5e961c3b3
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-02-11 04:39:56 +0000

    first commit + images

---
+++ main.js
@@ -0,0 +1,6 @@
+let images = [
+    "https://images.unsplash.com/photo-1558981806-ec527fa84c39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    "https://images.unsplash.com/photo-1581293738311-c1e14c2513dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    "https://images.unsplash.com/photo-1581337204873-ef36aa186caa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1038&q=80"
+]
+
